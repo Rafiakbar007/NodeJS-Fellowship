acad09c3583b913d3f70c8df26cd77b4e6a8a90e
Rafia Akbar <rafiaakbar007@gmail.com>
2026-07-27 23:58:44 +0500

    redeploy

--- MERNcrudProject/student-sphere/server/index.js
+++ MERNcrudProject/student-sphere/server/index.js
@@ -17,7 +17,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors({
   origin:[ "http://localhost:5173",
-     "https://YOUR-VERCEL-URL.vercel.app",
+      "https://node-js-fellowship.vercel.app",
   ],
           credentials: true,
 }));
