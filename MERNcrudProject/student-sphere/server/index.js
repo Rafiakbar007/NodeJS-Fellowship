1220ed49f9fb45f7330914b2243a120121fde834
Rafia Akbar <rafiaakbar007@gmail.com>
2026-07-28 00:23:46 +0500

    deployement

--- MERNcrudProject/student-sphere/server/index.js
+++ MERNcrudProject/student-sphere/server/index.js
@@ -20,6 +20,8 @@ app.use(cors({
       "https://node-js-fellowship.vercel.app",
   ],
           credentials: true,
+           methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
 }));
 
 app.use(express.json());
