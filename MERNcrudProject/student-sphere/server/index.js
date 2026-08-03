aa9eb6b73f54510e969392cf8b7de67f20d64268
Rafia Akbar <rafiaakbar007@gmail.com>
2026-08-03 21:10:33 +0500

    cors link update

--- MERNcrudProject/student-sphere/server/index.js
+++ MERNcrudProject/student-sphere/server/index.js
@@ -17,7 +17,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors({
   origin:[ "http://localhost:5173",
-      "https://node-js-fellowship.vercel.app",
+      "node-js-fellowship.vercel.app",
   ],
           credentials: true,
            methods: ["GET", "POST", "PUT", "DELETE"],
