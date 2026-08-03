924ccfd90288923175b101e75dbaafa3f0f36209
Rafia Akbar <rafiaakbar007@gmail.com>
2026-08-03 21:18:04 +0500

    cors fix

--- MERNcrudProject/student-sphere/server/index.js
+++ MERNcrudProject/student-sphere/server/index.js
@@ -16,13 +16,15 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin:[ "http://localhost:5173",
-      "node-js-fellowship.vercel.app",
+  origin: [
+    "http://localhost:5173",
+    "https://node-js-fellowship.vercel.app",
   ],
-          credentials: true,
-           methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
 }));
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
